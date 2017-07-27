7b18aa265468a93e85d841a3d5a706d3fb215bf9
dbaskakov <dm.baskakov@yandex.ru>
2017-07-27 12:22:05 +0300

    add initDb & populateDb

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,7 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT  INTO meals (userId,datetime, description, calories) VALUES
+  (100000,TIMESTAMP '2017-05-16 10:00:38','test1',500),
+  (100000,TIMESTAMP '2017-05-16 12:00:38','test2',500);
